95dcf8171f1f6c362b1f3fca7de75f319e314dc5
Lenovo-User <Lenovo-User@DESKTOP-KGOC38Q>
2024-01-24 18:28:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number (1-3): ");
+            int number = int.Parse(Console.ReadLine()); //same as Conver.ToInt32
+
+            switch(number)
+            {
+                case 1:
+                    Console.WriteLine("You entered '1'");
+                    break;
+                case 2:
+                    Console.WriteLine("You enetered '2'");
+                    break;
+                case 3:
+                    Console.WriteLine("You entered '3'");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 3. ");
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
